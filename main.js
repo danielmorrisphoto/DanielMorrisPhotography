97d404722a543c1d61c33a6b570a02576db69737
Daniel Morris <daniel@danielmorrisphoto.com>
2017-06-06 20:55:46 -0400

    various pages added modal works but needs tweaking. scroll reveal added but can be tweaked.

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
 
 
-$('.pop').on('click', function() {
-	$('.imagepreview').attr('src', $(this).find('img').attr('src'));
-	$('#imagemodal').modal('show');  
-});	
+// $('.pop').on('click', function() {
+// 	$('.imagepreview').attr('src', $(this).find('img').attr('src'));
+// 	$('#imagemodal').modal('show');  
+// });	
 
-		
+$(document).ready(function() {
+	$(".image-responsive").fancybox({
+		minWidth:"800px",
+		minHeight:"800px",
+		autoCenter:true, 	
+		aspectRatio:true,
+		modal:true,
 
+	});
+});
+
+window.sr = ScrollReveal({ duration: 2000 });
+sr.reveal('.reveal-flow', {
+
+}, 50);
 
